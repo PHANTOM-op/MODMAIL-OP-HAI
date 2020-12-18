3135bb620d11898b724747a937146ce632577312
LE PHANTOM <73237066+PHANTOM-op@users.noreply.github.com>
2020-12-18 19:42:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ async def on_ready():
 	print("The bot is online!")
 	bot.load_extension("cogs.onMessage")
 
-bot.run(os.environ.get("TOKEN"))
+bot.run(os.environ.get("NzczNzUzOTU0NjIyMjQyODE3.X6N0hg.6HJAtO0x5xzZ_q7YpVl01-6l3zI"))
